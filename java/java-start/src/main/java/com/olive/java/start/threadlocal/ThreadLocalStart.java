741ae8dad5371168694c12ea3375e5fb7786ffe3
Clare <tangqiang.dong@happy-seed.com>
2021-05-13 15:45:49 +0800

    threadlocal start

---
+++ java/java-start/src/main/java/com/olive/java/start/threadlocal/ThreadLocalStart.java
@@ -0,0 +1,10 @@
+package com.olive.java.start.threadlocal;
+
+/**
+ * @description: ThreadLocal
+ * @program: olive
+ * @author: dtq
+ * @create: 2021/5/13 15:42
+ */
+public class ThreadLocalStart {
+}
