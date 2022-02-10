0b4b7b7b3baaed6773c085cd65969d80fe9691ba
ClareTung <dongtangqiang@yeah.net>
2022-02-10 21:26:52 +0800

    batch初始化

---
+++ springboot/springboot-batch/src/main/java/com/olive/batch/OliveBatchApplication.java
@@ -0,0 +1,14 @@
+package com.olive.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dongtangqiang
+ */
+@SpringBootApplication
+public class OliveBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OliveBatchApplication.class, args);
+    }
+}
